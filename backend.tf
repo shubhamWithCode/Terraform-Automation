32b57d13493dd6b024194d14ebd026c5f86dcfa6
shubham <vickygaikwad41996@gmail.com>
2024-01-01 23:53:36 +0530

    my changes

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-batch-21"
-    key = "main"
+    bucket = "shubham-sg-bucket-1"
+    key = "Key"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
