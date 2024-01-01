7b8682b676e3f41f3ae91eff54505a558e2f3eca
shubham <vickygaikwad41996@gmail.com>
2024-01-02 00:00:50 +0530

    added av

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "shubham-sg-bucket-1"
     key = "Key"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
