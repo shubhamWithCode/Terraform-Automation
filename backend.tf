304810cd0ebe15c387e79d448072700eb599453e
shubham <vickygaikwad41996@gmail.com>
2024-01-05 00:01:50 +0530

    INC002 updated dynamodb table name from my-dynamodb-table to nike_dynamodb_table

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "shubhams-s3-bucket"
     key = "main"
     region = "ap-south-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "nike_dymanodb_table"
   }
 }
