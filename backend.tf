4374510d1518d645e936a73482ae1bc8f4b21bb0
Shubham Gaikwad <121483086+shubhamWithCode@users.noreply.github.com>
2025-03-15 12:04:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "shubhams-s3-bucketi"
     key = "main"
-    region = "ap-south-1"
+    region = "us-east-1"
     dynamodb_table = "nike_dymanodb_table"
   }
 }
